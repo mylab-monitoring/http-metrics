d02d4cd16ce91a3cf171770f35dd83620af77ac1
Andrey O <ozzy.ext@gmail.com>
2021-04-30 14:46:50 +0300

    Add EnumConverter

---
+++ src/MyLab.HttpMetrics/EnumNameConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLab.HttpMetrics
+{
+    /// <summary>
+    /// Converts <see cref="Enum"/> for metrics
+    /// </summary>
+    public static class EnumConverter
+    {
+        public static string ToLabel(Enum enumVal)
+        {
+            var items = new List<string>();
+            var enumString = enumVal.ToString();
+
+            StringBuilder buff = null;
+            char? lastChar = null;
+
+            for (int i = 0; i < enumString.Length; i++)
+            {
+                char ch = enumString[i];
+
+                if(!char.IsLetterOrDigit(ch)) continue;
+
+                if (char.IsUpper(ch) || (char.IsDigit(ch) && lastChar.HasValue && !char.IsDigit(lastChar.Value)))
+                {
+                    ApplyBuffer();
+                }
+
+                buff ??= new StringBuilder();
+                
+                buff.Append(char.ToLower(ch).ToString());
+
+                lastChar = ch;
+            }
+
+            if (buff != null && buff.Length != 0)
+                items.Add(buff.ToString());
+
+            return string.Join('_', items);
+
+            void ApplyBuffer()
+            {
+                if (buff != null && buff.Length != 0)
+                    items.Add(buff.ToString());
+
+                buff = new StringBuilder();
+            }
+
+        }
+    }
+}
---
+++ src/UnitTests/EnumConverterBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyLab.HttpMetrics;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EnumConverterBehavior
+    {
+        [Theory]
+        [InlineData(TestEnum.Foo, "foo")]
+        [InlineData(TestEnum.Foo1, "foo_1")]
+        [InlineData(TestEnum.Foo2Bar, "foo_2_bar")]
+        [InlineData(TestEnum.FooBar, "foo_bar")]
+        [InlineData(TestEnum.Foo | TestEnum.FooBar, "foo_foo_bar")]
+        [InlineData((TestEnum)50, "50")]
+        public void ShouldConvertEnumToLabel(TestEnum enumVal, string expected)
+        {
+            //Arrange
+
+
+            //Act
+            var actual = EnumConverter.ToLabel(enumVal);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Flags]
+        public enum TestEnum
+        {
+            Foo = 1,
+            Foo1 = 2,
+            Foo2Bar = 4,
+            FooBar = 8
+        }
+    }
+}
